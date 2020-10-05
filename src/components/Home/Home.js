7486a2441cec95e15f46c9767198a3d53a5b4512
AKSHAY <akshayjadhav461999@gmail.com>
2020-10-05 15:32:19 +0530

    Home component layout

---
+++ src/components/Home/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import BaseLayout from "../Base Layout/BaseLayout";
+
+function Home() {
+  return (
+    <BaseLayout title="Home">
+      <div className="home">
+        <h1>Home</h1>
+      </div>
+    </BaseLayout>
+  );
+}
+
+export default Home;
