77ee55bfcc74bf67e390af9e09d154e31231fc7f
AKSHAY <akshayjadhav461999@gmail.com>
2020-10-05 15:31:43 +0530

    navigation included

--- src/components/Base Layout/BaseLayout.js
+++ src/components/Base Layout/BaseLayout.js
@@ -1,21 +1,25 @@
 import React from "react";
 import "./BaseLayout.css";
 import { Container, Typography } from "@material-ui/core";
-function BaseLayout({ title = "Title", children = <p>AKSHAY</p> }) {
+import Navigation from "../Navigation/Navigation";
+function BaseLayout({ title = "Title", children }) {
   return (
-    <Container className="baseLayout">
-      <div className="baseLayout__container">
-        <Typography className="baseLayout__title" align="center" variant="h4">
-          {title}
-        </Typography>
-        <div className="baseLayout__content">{children}</div>
-        <footer className="baseLayout__footer">
-          <Container>
-            <p>CREATED BY AKS USING MERN AND REDUX TOOLKIT</p>
-          </Container>
-        </footer>
-      </div>
-    </Container>
+    <>
+      <Navigation />
+      <Container className="baseLayout">
+        <div className="baseLayout__container">
+          <Typography className="baseLayout__title" align="center" variant="h4">
+            {title}
+          </Typography>
+          <div className="baseLayout__content">{children}</div>
+          <footer className="baseLayout__footer">
+            <Container>
+              <p>CREATED BY AKS USING MERN AND REDUX TOOLKIT</p>
+            </Container>
+          </footer>
+        </div>
+      </Container>
+    </>
   );
 }
 
