0ffdae59a3d42f18d14acc07d0c633aab57459c0
AKSHAY <akshayjadhav461999@gmail.com>
2020-10-05 18:10:05 +0530

    route for sign up

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./components/Home/Home";
+import Signup from "./components/Signup/Signup";
 
 function App() {
   return (
     <div className="app">
       <Router>
         <Route path="/" exact component={Home} />
+        <Route path="/signup" exact component={Signup} />
       </Router>
     </div>
   );
