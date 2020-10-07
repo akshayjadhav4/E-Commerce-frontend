a86cae8613214e26a1fc5de0ee023bd9394e81a2
AKSHAY <akshayjadhav461999@gmail.com>
2020-10-07 14:36:24 +0530

    admin dashboard layout

---
+++ src/components/AdminDashboard/AdminDashboard.js
@@ -0,0 +1,14 @@
+import React from "react";
+import BaseLayout from "../Base Layout/BaseLayout";
+
+function AdminDashboard() {
+  return (
+    <BaseLayout title="Admin Dashboard">
+      <div className="adminDashboard">
+        <h1>AdminDashboard</h1>
+      </div>
+    </BaseLayout>
+  );
+}
+
+export default AdminDashboard;
