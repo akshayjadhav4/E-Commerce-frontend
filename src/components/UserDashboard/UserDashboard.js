a633762b931bc6bfcff66f367185e3cef3345e76
AKSHAY <akshayjadhav461999@gmail.com>
2020-10-07 14:30:27 +0530

    user dashboard layout

---
+++ src/components/UserDashboard/UserDashboard.js
@@ -0,0 +1,14 @@
+import React from "react";
+import BaseLayout from "../Base Layout/BaseLayout";
+
+function UserDashboard() {
+  return (
+    <BaseLayout title="User Dashboard">
+      <div className="userDashboard">
+        <h1>UserDashboard</h1>
+      </div>
+    </BaseLayout>
+  );
+}
+
+export default UserDashboard;
